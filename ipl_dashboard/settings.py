6800390df8858fdd6d54a974963aa7db9b1122f9
Darshan Bhavsar <darshanbhavsar4.db@gmail.com>
2021-08-25 22:47:31 +0530

    Update settings.py
    debug = true and static files changes

--- ipl_dashboard/settings.py
+++ ipl_dashboard/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a19uk3-3!4yp2la*rmx9j(o*4tb@y9ly^ib*h5=)zk+br&rr4b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
